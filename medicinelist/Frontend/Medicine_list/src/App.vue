2001cfef80aaa99764acdec2f9977f1900ed96d3
aticode <153619619+ati20030921@users.noreply.github.com>
2025-07-09 18:26:16 +0200

    Add files via upload
    Project

---
+++ medicinelist/Frontend/Medicine_list/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld />
+      <nav class="navbar navbar-expand-sm bg-dark navbar-light">
+        <div class="container-fluid">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <RouterLink to="/" class="nav-link text-light">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink to="/application" class="nav-link text-light" >Appliaction</RouterLink>
+            </li>
+          </ul>
+          <div>
+          <button src="../src/views/Registration.vue" class="btn btn-primary" type="button">Login / Registration</button>          
+            </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+.navbar{
+  height: 100px;
+}
+
+.nav-link{
+  font-size: 25px;
+}
+
+.btn{
+  background-color: black;
+  color: white;
+  height: 60px;
+  font-size: 25px;
+}
+</style>
+
+
---
+++ medicinelist/README.md
@@ -0,0 +1,20 @@
+# 💊 Gyógyszer Nyilvántartó Projekt
+
+📝 **Leírás**  
+Ez a projekt egy gyógyszer-nyilvántartó rendszer, amely lehetővé teszi a gyógyszerek listázását.
+
+🔧 **Technológiák**
+- 🌐 **Frontend**: HTML, CSS, Bootstrap, React, Vue
+- 🖥️ **Backend**: PHP, Java
+- 💾 **Adatbázis**: MySQL vagy MariaDB
+
+✨ **Fő funkciók**
+- 👤 Felhasználói regisztráció
+- 🔐 Felhasználói belépés (login)
+- 📋 Gyógyszerek listázása
+- ➕ Új gyógyszer felvitele
+- ✏️ Gyógyszer adatainak szerkesztése
+- 🗑️ Gyógyszer törlése
+
+🧑‍💻 **Fejlesztők**: *Jegenyés Bence, Daroczi Attila*  
+📧 **Kapcsolat**: *banfiattila134@gmail.com*
