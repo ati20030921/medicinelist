e1b721f1093d0a276c9391399b79f73feb65ecf0
Attila <szt3daratt@vasvari.org>
2025-07-09 18:14:16 +0200

    h1

--- Frontend/Medicine_list/src/App.vue
+++ Frontend/Medicine_list/src/App.vue
@@ -6,6 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <HelloWorld />
+      <h1>HUH</h1>
 
       <nav class="navbar navbar-expand-sm bg-dark navbar-light">
         <div class="container-fluid">
